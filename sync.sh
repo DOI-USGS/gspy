bf05681b62a688cd9fbdb842b5420c8212ad9c37
Leon Foks <nfoks@contractor.usgs.gov>
2026-03-30 13:23:50 -0700

    new sync

--- sync.sh
+++ sync.sh
@@ -1,3 +1,9 @@
-cd ../gspy.git
-git fetch secondary
-git push origin
+cd ../usgs_gspy
+git checkout master
+git fetch gspy master
+git merge gspy/master
+git push origin master
+git checkout develop
+git fetch gspy develop
+git merge gspy/develop
+git push origin develop
